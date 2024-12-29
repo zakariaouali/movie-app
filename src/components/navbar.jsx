0a9a0323d2da0ba8615c87e885d2167afe9ac992
Zakaria Ouali <oualizakaria5@gmail.com>
2024-12-29 12:19:03 +0100

    secondd

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,10 +3,10 @@ import "../css/Navbar.css";
 export default function NavBar(){
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/" ><img src="movielogo.png" alt="" width='70px' />Zak4movie</Link>
+            <Link to="/movie-app" ><img src="movielogo.png" alt="" width='70px' />Zak4movie</Link>
         </div>
         <div className="navbar-links">
-            <Link className="nav-link" to="/" >Home</Link>
+            <Link className="nav-link" to="/movie-app" >Home</Link>
             <Link to="/favorites" className="nav-link" >Favorites</Link>
         </div>
     </nav>
